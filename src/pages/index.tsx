7b4a2c23ea426ddd400bc26a5b607cc0ee709e3a
alemedinabjj <101877712+alemedinabjj@users.noreply.github.com>
2022-09-06 16:33:42 -0300

    ajusts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,24 @@
-import { Header } from "../components/Header/Index"
 
-export default function Home() {
+export default function Home(props) {
+
+  console.log(props.episodes)
+
   return (
     <>
       <h1>index</h1>
     </>
   )
+}
+
+export const getStaticProps = async () => {
+  const response = await fetch("http://localhost:3333/episodes")
+  const data = await response.json()
+
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidade: 60 * 60 * 8,
+  }
 }
