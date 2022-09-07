efba7e741d7dd1a5ea624bb9b52c1739ad3ae961
alemedinabjj <101877712+alemedinabjj@users.noreply.github.com>
2022-09-06 23:19:30 -0300

    getstaticprops

---
+++ src/pages/episodes/[slug].tsx
@@ -0,0 +1,107 @@
+import { GetStaticPaths, GetStaticProps } from 'next'
+import { useRouter } from 'next/router'
+import styles from './episode.module.scss'
+import React from 'react'
+import { api } from '../../services/api'
+import { ConvertDurationToTimeString } from '../../utils/ConvertDurationToTimeString'
+import Image from 'next/image'
+
+type Episode = {
+  id: string
+  title: string
+  members: string
+  thumbnail: string
+  description: string
+  duration: number
+  durationAsString: string
+  url: string
+  publishedAt: string
+}
+
+type HomeProps = {
+  episode: Episode
+}
+
+const Episode = ({ episode }: HomeProps) => {
+  const router = useRouter()
+
+  return (
+    <div className={styles.episode}>
+      <div className={styles.thumbnailContainer}>
+        <button type="button" onClick={() => router.back()}>
+          <img src="/arrow-left.svg" alt="Voltar" />
+        </button>
+        <Image
+          width={700}
+          height={160}
+          objectFit="cover"
+          src={episode.thumbnail}
+          alt={episode.title}
+        />
+        <button type="button">
+          <img src="/play.svg" alt="Tocar episódio" />
+        </button>
+      </div>
+      <header>
+        <h1>{episode.title}</h1>
+        <span>{episode.members}</span>
+        <span>{episode.publishedAt}</span>
+        <span>{episode.durationAsString}</span>
+      </header>
+      <div
+        className={styles.description}
+        dangerouslySetInnerHTML={{ __html: episode.description }}
+      />
+    </div>
+  )
+}
+
+export default Episode
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const response = await api.get('episodes', {
+    params: {
+      _limit: 2,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
+
+  const paths = response.data.map(episode => {
+    return {
+      params: {
+        slug: episode.id
+      }
+    }
+  })
+
+  return {
+    paths,
+    fallback: 'blocking'
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ctx => {
+  const { slug } = ctx.params
+
+  const { data } = await api.get(`/episodes/${slug}`)
+
+  const episode = {
+    id: data.id,
+    title: data.title,
+    thumbnail: data.thumbnail,
+    members: data.members,
+    publishedAt: data.published_at,
+    duration: Number(data.file.duration),
+    durationAsString: ConvertDurationToTimeString(Number(data.file.duration)),
+    description: data.description,
+    url: data.file.url
+  }
+
+  return {
+    props: {
+      episode
+    },
+    revalidate: 60 * 60 * 24 // 24 hours
+  }
+}
