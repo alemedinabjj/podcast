5e1cfa1b6d4ef6205c92720340a66b1c82d320fc
alemedinabjj <101877712+alemedinabjj@users.noreply.github.com>
2022-09-07 17:46:42 -0300

    api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,8 @@
 import axios from 'axios'
 
+
+const baseURL = 'https://my-json-server.typicode.com/pereirafi/podcastrnext/'
+
 export const api = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/pereirafi/podcastrnext/'
+  baseURL
 })
